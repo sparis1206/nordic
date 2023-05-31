f4961d492d2d034a5b2973c512bb38e74d52ddd0
Simon <simon.paris1206@gmail.com>
2023-05-31 14:57:16 +0200

    Initial Nordic

---
+++ examples/TP_ELEC_NUM/TP_ELEC_NUM/pca10056/blank/ses/Output/Release/Obj/TP_ELEC_NUM/app_util_platform.d
@@ -0,0 +1,26 @@
+Output/Release/Obj/TP_ELEC_NUM/app_util_platform.o: \
+ C:\Users\simon\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\util\app_util_platform.c \
+ C:\Users\simon\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\util\app_util_platform.h \
+ D:/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdint.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ C:\Users\simon\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\util\nrf_assert.h \
+ C:\Users\simon\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\util\app_error.h \
+ D:/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdio.h \
+ D:/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/__crossworks.h \
+ D:/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdbool.h \
+ C:\Users\simon\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\util\sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ C:\Users\simon\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\util\nordic_common.h \
+ C:\Users\simon\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\util\app_error_weak.h
